4277165a834dee31e99cfe56b0ecdbafbdd553f8
stoneThree <1571681321@qq.com>
2022-08-22 22:29:22 +0800

    hot-fix commit1

--- src/main/java/com/stoneThree/GitTest.java
+++ src/main/java/com/stoneThree/GitTest.java
@@ -12,5 +12,6 @@ package com.stoneThree;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hot-fix");
     }
 }
